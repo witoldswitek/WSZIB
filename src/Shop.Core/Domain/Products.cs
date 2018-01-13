7b05aaa30a1bc5be85966a9b7a194525faa7245b
Witold Michał Świtek <switek@student.wszib.edu.pl>
2018-01-13 12:18:33 +0100

    Project cleanup

---
+++ src/Shop.Core/Domain/Products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Core.Domain
+{
+    class Products
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public string Category { get; set; }
+        public decimal Price { get; }
+
+
+        public Products(string name, string category, decimal price)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Category = category;
+            Price = price;
+        }
+    }
+}
